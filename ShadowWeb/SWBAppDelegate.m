a735e8f6141ab2bfeefdfe984245b3a780f0e1cb
zhangfan <twotrees.zf@gmail.com>
2015-06-06 03:38:23 +0800

    remove unuse files

--- ShadowWeb/SWBAppDelegate.m
+++ ShadowWeb/SWBAppDelegate.m
@@ -5,8 +5,6 @@
 //  Created by clowwindy on 2/16/13.
 //  Copyright (c) 2013 clowwindy. All rights reserved.
 //
-#import <Crashlytics/Crashlytics.h>
-
 #import "GZIP.h"
 #import "AppProxyCap.h"
 #import "SWBAppDelegate.h"
@@ -39,8 +37,6 @@ void polipo_exit();
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     [self updateProxyMode];
-
-    [Crashlytics startWithAPIKey:@"fa65e4ab45ef1c9c69682529bee0751cd22d5d80"];
 
     [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:^{
 
